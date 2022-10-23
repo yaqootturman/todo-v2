ff3afabe9230fe1887d268eddb8e2657a4883a5c
yaqootturman <yaqoot.turman@gmail.com>
2022-10-23 12:11:06 +0300

    delete functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,23 @@
-import './App.css';
-import { useState } from 'react';
-import Form from './components/form/form.component';
-import List from './components/list/list.component';
-
+import "./App.css";
+import { useState } from "react";
+import Form from "./components/form/form.component";
+import List from "./components/list/list.component";
 
 function App() {
-  const  [items, setItems] = useState([])
+  const [items, setItems] = useState([]);
 
-  const addItem=(item)=>{
-    setItems([...items, item])
-  }
+  const addItem = (item) => {
+    setItems([...items, item]);
+  };
+  const deleteItem = (id) => {
+    const newItems = items.filter((ele) => ele.id !== id);
+    setItems(newItems);
+  };
 
   return (
     <div className="App">
-      <Form onAddItem={addItem}/>
-      <List items={items}/>
+      <Form onAddItem={addItem} />
+      <List items={items} onDelete={deleteItem} />
     </div>
   );
 }
--- src/components/list/list.component.jsx
+++ src/components/list/list.component.jsx
@@ -1,12 +1,12 @@
 import Item from "./item.componemt";
 import "./list.css";
 const List = (props) => {
-  const { items } = props;
+  const { items, onDelete } = props;
   return (
     <div className="items-list">
       <ul>
         {items.map((ele) => {
-          return <Item item={ele} key={ele.id} />;
+          return <Item item={ele} key={ele.id} deleteItem={onDelete} />;
         })}
       </ul>
     </div>
