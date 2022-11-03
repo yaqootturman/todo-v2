455677368f9e29fe7c840057cebce791b0708582
yaqootturman <yaqoot.turman@gmail.com>
2022-11-03 14:49:31 +0200

    add font-awsome with react

--- src/components/list/item.componemt.jsx
+++ src/components/list/item.componemt.jsx
@@ -1,14 +1,27 @@
 import "./list.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faCheck,
+  faTrash,
+} from "@fortawesome/free-solid-svg-icons";
+
 const Item = (props) => {
   const { item, deleteItem, onFinish } = props;
   return (
     <li className="item">
-      <div className={item.isDone?"done-item":"title"}>
+      <div className={item.isDone ? "done-item" : "title"}>
         <h1>{item.title}</h1>
       </div>
       <div className="actions">
-        <button onClick={() => deleteItem(item.id)}>Delete</button>
-        <button onClick={() => onFinish(item.id)}>DONE</button>
+        <button type="button" onClick={() => deleteItem(item.id)}>
+          {" "}
+          <FontAwesomeIcon icon={faTrash} />
+          Delete
+        </button>
+        <button type="button" onClick={() => onFinish(item.id)}>
+          <FontAwesomeIcon icon={faCheck} />
+          DONE
+        </button>
       </div>
     </li>
   );
